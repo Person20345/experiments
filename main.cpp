24a65955f63cc5aee5e749569b45831a44ccd33a
p2034 <45317009+Person20345@users.noreply.github.com>
2021-10-29 22:12:43 +0300

    delete someone else's code

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 
 #include "src/constants.h" // must compile with "src/constants.cpp"
-#include "src/aes.h"
+#include "examples/crypto.h"
+#include "src/crc32.h"
 
 
 int main() {
@@ -14,29 +15,6 @@ int main() {
 
 	// (get/print)-functions:
 
-  unsigned char plain[] = "text_for_my_friends_and_me_test_aes";
-  unsigned char key[] = "key";
-  unsigned int plainLen = 35 * sizeof(unsigned char);  //bytes in plaintext
-  unsigned int outLen = 0;  // out param - bytes in сiphertext
-
-  AES aes(256);  //128 - key length, can be 128, 192 or 256
-
-  std::cout << "plainLen = " << std::dec << plainLen << "\n";
-  for (int i = 0; i < plainLen; i++)
-    std::cout << std::hex << int(plain[i]) << " ";
-  std::cout << "\n\n";
-  
-  unsigned char * ec = aes.EncryptECB(plain, plainLen, key, outLen);
-  std::cout << "outLen = " << std::dec << outLen << "\n";
-  for (int i = 0; i < outLen; i++)
-    std::cout << std::hex << int(ec[i]) << " ";
-  std::cout << "\n\n";
-
-
-  unsigned char * dc = aes.DecryptECB(ec, outLen, key);
-  for (int i = 0; i < outLen; i++)
-    std::cout << dc[i] << "";
-  std::cout << "\n\n";
 
 	// end time counter
 	time(&end_time);
